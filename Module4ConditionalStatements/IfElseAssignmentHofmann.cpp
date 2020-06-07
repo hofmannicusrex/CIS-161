d92e2ea70993488c06fb7d29fbfec1368788821f
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-07 09:53:35 -0500

    Initial push

---
+++ Module4ConditionalStatements/IfElseAssignmentHofmann.cpp
@@ -0,0 +1,19 @@
+/*  IfElseAssignmentHofmann.cpp || Nick Hofmann 6/1/2020
+    nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+
+    This program will ask the user for a score between 1 and 10, determine if it is a
+    passing grade (70% or more is a passing grade). It then outputs a message stating
+    whether or not it is a passing grade to the user.
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+
+}// End of main method
