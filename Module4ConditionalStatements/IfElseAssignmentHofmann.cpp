2807cb1576958b3b95ae498ce459c40ebc542a1e
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-07 10:10:02 -0500

    Final working push

--- Module4ConditionalStatements/IfElseAssignmentHofmann.cpp
+++ Module4ConditionalStatements/IfElseAssignmentHofmann.cpp
@@ -1,4 +1,4 @@
-/*  IfElseAssignmentHofmann.cpp || Nick Hofmann 6/1/2020
+/*  IfElseAssignmentHofmann.cpp || Nick Hofmann 6/7/2020
     nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
 
     This program will ask the user for a score between 1 and 10, determine if it is a
@@ -12,8 +12,22 @@ using namespace std;
 int main()
 {
 
+    int userTestScore;
 
+    cout << "Please enter a test score between 1 and 10: ";
+    cin >> userTestScore;
 
+    if (userTestScore >= 7) {
+
+        cout << "\nThe test score of " << userTestScore << " is a passing grade!\n\n";
+
+    }
+    else {
+
+        cout << "\nThe test score of " << userTestScore << " is a failing grade :(\n";
+        cout << "...might as well join the circus I guess.\n\n";
+
+    }
     return 0;
 
 }// End of main method
