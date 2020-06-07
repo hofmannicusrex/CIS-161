d10466093c44b1634fa60c54d496e4bc2f92abda
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-07 14:55:57 -0500

    Initial push to Git

---
+++ Module4/WinslowGlassHofmann/WinslowGlassHofmann.cpp
@@ -0,0 +1,25 @@
+/*  WinslowGlassHofmann.cpp || Nick Hofmann 6/7/2020
+    nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+
+    This program will prompt the user for the length, width, and height of a box.
+    It will then calculate and return both the volume of the box and the surface
+    area of the box.
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // VARIABLE DECLARATIONS
+    const int BOTTLE_COST = 24;
+    int numberOfBottles;
+
+    cout << "Thanks for choosing Winslow Glass!\n\n";
+    cout << "How many bottles would you like to purchase? \n\n\n";
+    cin >> numberOfBottles;
+
+
+
+    return 0;
+}// End of main method
