487c20e92ecc4bf452bb583ec111034893ad7941
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-07-01 23:28:53 -0500

    Initial push to Git

---
+++ Module11/CarAuctionHouse/CarAuctionHouse.cpp
@@ -0,0 +1,24 @@
+/*  CarAuctionHouse.cpp || Nick Hofmann 7/1/2020
+    nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+
+    This program will .
+
+*/
+#include <iostream>
+using namespace std;
+
+class AuctionHouse
+{
+private:
+    string ID;
+    int sold;
+    int bluebook;
+};
+
+int main()
+{
+
+
+    
+    return 0;
+}  // End of main method
